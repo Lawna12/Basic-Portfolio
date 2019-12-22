725d724c319afdbdfe7d71194d8fc8963ff1be65
AndrewL <lawna12@gmail.com>
2019-12-21 20:24:04 -0500

    timer added

--- codeQuiz/main.js
+++ codeQuiz/main.js
@@ -88,3 +88,15 @@ var questions = [
 var quiz = new Quiz(questions);
 
 populate();
+
+var sec = 15;
+var time = setInterval(myTimer, 1000);
+
+function myTimer() {
+    document.getElementById('timer').innerHTML = sec + "sec left";
+    sec--;
+    if (sec == -1) {
+        clearInterval(time);
+        alert("Time out!! :(");
+    }
+}
