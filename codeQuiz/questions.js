74ada0ef4358b276ed7037e0c66670fa4b13f189
AndrewL <lawna12@gmail.com>
2020-01-23 17:29:10 -0500

    removing codeQuiz

--- codeQuiz/questions.js
+++
@@ -1,9 +0,0 @@
-function Questions(text, choices, answer) {
-    this.text = text;
-    this.choices = choices;
-    this.answer = answer;
-}
-
-Questions.prototype.correctAnswer = function(choices) {
-    return choice === this.answer
-}
